deabe12828b8496efed2fc9eac11fc5d045d8a32
CesarEdlm <160139296+CesarEdlm@users.noreply.github.com>
2024-02-29 09:03:04 -0600

    000_p1_hablar
    cambio de texto

--- 000_P1_Hablar_usuario_/000_P1_Hablar_usuario_.cpp
+++ 000_P1_Hablar_usuario_/000_P1_Hablar_usuario_.cpp
@@ -1,19 +1,16 @@
-// 000_P1_Hablar_usuario_.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// 000_P1_Hablar_usuario_.cpp 
+//cesar de loera
+//hablaremos con el usuario
 
 #include <iostream>
-
+#include <locale>
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    setlocale(LC_ALL, "es_MX.UTF-8");
+     
+    std::cout << "\nHey there, little stranger\n" <<
+        "Questions i need to ask";
 
+}
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
 // Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
