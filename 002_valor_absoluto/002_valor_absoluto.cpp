3a02212723821d1d3a7063595e1619b1c7d6191e
CesarEdlm <zap1079@amerike.edu.mx>
2024-04-22 07:56:46 -0600

    valor absoluto
    final valor absoluto

--- 002_valor_absoluto/002_valor_absoluto.cpp
+++ 002_valor_absoluto/002_valor_absoluto.cpp
@@ -1,32 +1,23 @@
-// 002_valor_absoluto.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+
 
 #include <iostream>
 #include <cmath>
 #include <locale>
 int main()
-{
-    bool:
-    std::cout << "Ingresa un número: ";
-    std::cin >> numero;
+//se debe de encontrar el valor absoluto de un numero,para encontrar dicho valor debo de hacer que el numero positivo se mantenga asi y un numero negativo pase a positivo
+//datos de entrada: numero
+//datos de salida: valor absoluto
+//proceso=numero *(-1)
 
-    // Convierte el número negativo a positivo
-    if (numero < 0) {
-        numero = -numero;
+{
+  
+    double numero;
+    std::cout << "ingresa tu numero en negativo";
+    std::cin >> numero;
+    double numpos = numero * (-1);
+
+    std::cout << " tu numero negativo ahora esta en positivo " << numpos << std::endl ;
+
+  
     }
 
-    std::cout << "El valor absoluto es: " << numero << endl;
-    return 0;
-
-
-}
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
