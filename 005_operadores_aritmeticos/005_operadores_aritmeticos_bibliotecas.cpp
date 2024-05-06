aca52b9dd6e88f6942b5e98ab72e7a929552d7b7
CesarEdlm <zap1079@amerike.edu.mx>
2024-05-06 08:41:23 -0600

    005
    practica 005 terminada

---
+++ 005_operadores_aritmeticos/005_operadores_aritmeticos_bibliotecas.cpp
@@ -0,0 +1,13 @@
+// 005_operadores_aritmeticos_bibliotecas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+
+
+}
+
